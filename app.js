e8fc1158c6987ff1ae9c0b642bf1100ae59c43ee
Debasis-Behera4143 <debasisbehera229@gmail.com>
2025-08-15 21:54:36 +0530

    commited

---
+++ app.js
@@ -0,0 +1,10 @@
+let btn = document.querySelector("button");
+let input = document.querySelector("input");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click", function() {
+    let li = document.createElement("li");
+    li.innerText = input.value;
+    ul.appendChild(li);
+    input.value = ""; // Clear the input field after adding the item
+});
